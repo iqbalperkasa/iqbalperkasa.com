4983668253f9200eea4c76f980bdf05d8e0f66a2
Iqbal Perkasa <iqbalperkasa@gmail.com>
2021-05-25 22:03:56 +0700

    initial move to vite

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+div
+  main(class='main-content')
+    h1 Iqbal Perkasa
+    div(class='space space-small')
+    p Web product developer
+    p Software engineer
+    div(class='space')
+    p(class='title') Currently
+    p
+      | Frontend engineer,&nbsp;
+      a(href='https://www.linkedin.com/company/pt-bukalapak-com') Bukalapak
+    div(class='space')
+    p(class='title') Residence
+    p Jakarta/Bandung, Indonesia
+    div(class='space')
+    p(class='title') Stacks
+    p Vue, JavaScript, Express, Sass
+    div(class='space')
+    p(class='title') Handles
+    p(class='handles')
+      a(href='https://github.com/iqbalperkasa') GitHub
+      a(href='https://twitter.com/iqbalperkasa') Twitter
+      a(href='https://www.linkedin.com/in/iqbalperkasa') LinkedIn
+      a(href='https://iqbalperkasa.com/resume') Resume
+      a(href='https://iqbalperkasa.com/portfolio') Portfolio
+      //- a(href='https://www.instagram.com/iqbalperkasa') Instagram
+</template>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import '@/css/tailwind.css';
+import '@/css/style.stylus';
+
+createApp(App).mount('#app');

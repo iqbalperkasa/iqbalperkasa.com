eb26a59a085ca9fbc91d91d544aa6c84c3bda5db
Iqbal Perkasa <iqbalperkasa@gmail.com>
2021-11-04 21:05:00 +0700

    update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
+var CACHE = 'iqbalperkasa-com-cache';
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open('airhorner').then((cache) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => {
     return cache.addAll([
       '/',
       '/style.css',
@@ -8,11 +10,37 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  // console.log(event.request.url);
+  event.respondWith(fromCache(event.request));
 
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request);
-    })
-  );
+  event.waitUntil(update(event.request).then(refresh));
 });
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request);
+  });
+};
+
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(() => {
+        return response;
+      });
+    });
+  });
+};
+
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      var message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag'),
+      };
+
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+};

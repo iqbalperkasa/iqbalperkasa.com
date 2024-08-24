1fdaedb21d1d198590e1bf9f461170451cc661b8
iqbalperkasa <iqbalperkasa@gmail.com>
2024-08-24 15:52:42 +0700

    Resize icon

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
     </section>
     <section class="flex gap-4 items-center">
       <a href="/resume.pdf" class="border-2 border-transparent bg-stone-200 hover:bg-stone-300 active:bg-stone-400 transition-colors py-3 px-4 rounded-lg font-medium">View my resume</a>
-      <a href="https://iqbalperkasa.super.site" target="_blank" class="border-2 border-transparent hover:border-stone-300 active:border-stone-400 transition-colors py-3 px-4 rounded-lg font-medium">Portfolio <Icon name="gravity-ui:arrow-up-right-from-square" class="align-middle mb-1 ml-1" size="16" /></a>
+      <a href="https://iqbalperkasa.super.site" target="_blank" class="border-2 border-transparent hover:border-stone-300 active:border-stone-400 transition-colors py-3 px-4 rounded-lg font-medium">Portfolio <Icon name="gravity-ui:arrow-up-right-from-square" class="align-middle mb-1 ml-1" size="18" /></a>
     </section>
     <section>
       <p class="lg:text-xl text-stone-600 lg:leading-loose leading-relaxed">
@@ -28,8 +28,8 @@
         <anchor href="https://t.me/iqbalperkasa">Telegram</anchor>, or<br />
         email at <anchor href="mailto:iqbal@iqbalperkasa.com" @click="copyToClipboard($event)">{{ email }}</anchor>
         <span class="ml-1 cursor-pointer" @click="copyToClipboard($event)">
-          <Icon v-show="!copied" size="16" name="gravity-ui:copy" class="align-middle" />
-          <Icon v-show="copied" size="16" name="gravity-ui:circle-check-fill" class="align-middle text-green-500" />
+          <Icon v-show="!copied" size="18" name="gravity-ui:copy" class="align-middle" />
+          <Icon v-show="copied" size="18" name="gravity-ui:circle-check-fill" class="align-middle text-green-500" />
         </span>
       </p>
     </section>

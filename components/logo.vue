72d0782d198447e353ead05a098e66dd24221287
iqbalperkasa <iqbalperkasa@gmail.com>
2024-08-24 09:29:44 +0700

    Logo component

---
+++ components/logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="inline-flex" :style="{ gap: grid + 'px' }">
+    <div class="bg-black" :style="{ width: grid + 'px', height: height + 'px' }"></div>
+    <div class="bg-black" :style="{ width: grid + 'px', height: height + 'px' }"></div>
+    <div class="bg-black" :style="{ width: grid + 'px', height: height / 2 + 'px' }"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  size: number,
+}>();
+const columns = 5;
+const grid = props.size / columns;
+const height = grid * columns;
+</script>

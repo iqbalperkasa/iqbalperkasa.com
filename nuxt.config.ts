90c67dfb4b554b073f366ae3cec759faca505cd7
Iqbal Perkasa <iqbalperkasa@gmail.com>
2024-08-24 09:41:32 +0700

    Revamp 2024 first update (#29)
    * Init nuxt
    
    * Upload avatar
    
    * Logo component
    
    * Anchor link componnet
    
    * Initiate revamp

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/google-fonts', "@nuxt/icon"],
+  typescript: {
+    typeCheck: true,
+  },
+  app: {
+    head: {
+      title: 'Iqbal Perkasa',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0', },
+        { name: 'description', content: 'Website of Iqbal Perkasa, a full-stack engineer from Indonesia.' },
+        { property: 'og:title', content: 'Iqbal Perkasa' },
+        { property: 'og:description', content: 'Website of Iqbal Perkasa, a full-stack engineer from Indonesia.' },
+        { property: 'og:url', content: 'https://iqbalperkasa.com' },
+        { property: 'og:type', content: 'article' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/avatar.png' },
+      ],
+      // script: [
+      //   {
+      //     async: true,
+      //     src: 'https://platform.twitter.com/widgets.js',
+      //   },
+      // ],
+    },
+  },
+  googleFonts: {
+    families: {
+      Inter: '400..600',
+    },
+    download: true,
+  },
+  icon: {
+    serverBundle: {
+      collections: ['gravity-ui'],
+    },
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        md: '3rem',
+      },
+      screens: {
+        md: '1080px',
+      },
+    },
+    fontFamily: {
+      'sans': ['Inter', 'system-ui', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+

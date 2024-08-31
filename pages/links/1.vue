6e112b0c7a1a58a6bb484342fb3c89c246cf6e61
iqbalperkasa <iqbalperkasa@gmail.com>
2024-08-31 07:39:31 +0700

    Update link 2

--- pages/links/1.vue
+++ pages/links/1.vue
@@ -1,1 +1,1 @@
-<script setup lang="ts">await navigateTo('https://gist.github.com/iqbalperkasa/6c5a4491fedac55103a0eeefc4efd5e3', { external: true })</script>
+<script setup lang="ts">await navigateTo('https://gist.github.com/iqbalperkasa/090fc68adb871566f0043ddd10abc339', { external: true })</script>

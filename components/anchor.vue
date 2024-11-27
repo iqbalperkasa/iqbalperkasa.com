69f704612095a0436bde519f77a77049a0af35e4
iqbalperkasa <iqbalperkasa@gmail.com>
2024-11-27 07:00:45 +0700

    Sort tailwind

--- components/anchor.vue
+++ components/anchor.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="border-b border-stone-500 border-dashed hover:border-solid cursor-pointer" v-bind="$attrs">
+  <a class="border-b border-dashed cursor-pointer border-stone-500 hover:border-solid" v-bind="$attrs">
     <slot></slot>
   </a>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="flex flex-col gap-16 p-8 lg:p-24 w-full 2xl:w-[120rem] 2xl:mx-auto min-w-[23.125rem]">
-    <header class="flex gap-6 items-center">
+    <header class="flex items-center gap-6">
       <logo v-if="showLogo" :size="20" @mouseover="showLogo = false" />
       <div v-else class="size-[20px] relative bg-stone-300 rounded-full" @mouseleave="showLogo = true">
         <div class="absolute size-[50px] left-[-15px] top-[-15px] bg-[url('/avatar.png')] bg-cover rounded-full cursor-none"></div>
@@ -8,19 +8,19 @@
       <!-- <img src="/avatar.jpg" class="rounded-full w-[40px]" /> -->
       <h1 class="text-xl font-medium leading-none">Iqbal Perkasa</h1>
       <!-- <nav class="flex content-center">
-        <nuxt-link class="text-md text-stone-400 hover:text-black active:bg-stone-100 transition-colors rounded-md p-4 cursor-pointer leading-none">Contact</nuxt-link>
+        <nuxt-link class="p-4 leading-none cursor-pointer transition-colors rounded-md text-md text-stone-400 hover:text-black active:bg-stone-100">Contact</nuxt-link>
       </nav> -->
     </header>
     <section class="relative">
       <!-- <div class="size-48 opacity-40 rounded-full rotate-12 bg-red-100 absolute -right-10 -top-28 z-0 bg-[url('/avatar.jpg')] bg-cover"></div> -->
       <p class="max-w-[60rem] md:max-w-[40rem] text-2xl leading-relaxed lg:text-4xl lg:leading-[1.8] relative font-medium z-10">I'm a full-stack web engineer focusing on frontend JavaScript ecosystem. I help companies and startups to build, design the system, plan, and architect the features to reach the goal which provide value<br class="hidden md:block" /> to the customers.</p>
     </section>
-    <section class="flex gap-4 items-center">
-      <a href="/resume.pdf" class="border-2 border-transparent bg-stone-200 hover:bg-stone-300 active:bg-stone-400 transition-colors py-3 px-4 rounded-lg font-medium">View my resume</a>
-      <a href="https://iqbalperkasa.super.site" target="_blank" class="border-2 border-transparent hover:border-stone-300 active:border-stone-400 transition-colors py-3 px-4 rounded-lg font-medium">Portfolio <Icon name="gravity-ui:arrow-up-right-from-square" class="align-middle mb-1 ml-1" size="18" /></a>
+    <section class="flex items-center gap-4">
+      <a href="/resume.pdf" class="px-4 py-3 font-medium border-2 border-transparent rounded-lg transition-colors bg-stone-200 hover:bg-stone-300 active:bg-stone-400">View my resume</a>
+      <a href="https://iqbalperkasa.super.site" target="_blank" class="px-4 py-3 font-medium border-2 border-transparent rounded-lg transition-colors hover:border-stone-300 active:border-stone-400">Portfolio <Icon name="gravity-ui:arrow-up-right-from-square" class="mb-1 ml-1 align-middle" size="18" /></a>
     </section>
     <section>
-      <p class="lg:text-xl text-stone-600 lg:leading-loose leading-relaxed">
+      <p class="leading-relaxed lg:text-xl text-stone-600 lg:leading-loose">
         Reach me on <br class="md:hidden" />
         <anchor href="https://www.linkedin.com/in/iqbalperkasa">LinkedIn</anchor>,
         <anchor href="https://twitter.com/iqbalperkasa">Twitter</anchor>,
@@ -29,7 +29,7 @@
         email at <anchor href="mailto:iqbal@iqbalperkasa.com" @click="copyToClipboard($event)">{{ email }}</anchor>
         <span class="ml-1 cursor-pointer" @click="copyToClipboard($event)">
           <Icon v-show="!copied" size="18" name="gravity-ui:copy" class="align-middle" />
-          <Icon v-show="copied" size="18" name="gravity-ui:circle-check-fill" class="align-middle text-green-500" />
+          <Icon v-show="copied" size="18" name="gravity-ui:circle-check-fill" class="text-green-500 align-middle" />
         </span>
       </p>
     </section>

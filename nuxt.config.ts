6e5315c98be44269e1a118ceac6644edf137f877
iqbalperkasa <iqbalperkasa@gmail.com>
2025-04-24 08:08:08 +0700

    Move vite config to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   // devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/google-fonts', "@nuxt/icon"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/icon"],
   // typescript: {
   //   typeCheck: true,
   // },
+  devServer: {
+    port: 1234,
+  },
   app: {
     head: {
-      title: 'Iqbal Perkasa',
+      title: "Iqbal Perkasa",
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0', },
-        { name: 'description', content: 'Website of Iqbal Perkasa, a full-stack engineer from Indonesia.' },
-        { property: 'og:title', content: 'Iqbal Perkasa' },
-        { property: 'og:description', content: 'Website of Iqbal Perkasa, a full-stack engineer from Indonesia.' },
-        { property: 'og:url', content: 'https://iqbalperkasa.com' },
-        { property: 'og:type', content: 'article' },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        {
+          name: "description",
+          content:
+            "Website of Iqbal Perkasa, a full-stack engineer from Indonesia.",
+        },
+        { property: "og:title", content: "Iqbal Perkasa" },
+        {
+          property: "og:description",
+          content:
+            "Website of Iqbal Perkasa, a full-stack engineer from Indonesia.",
+        },
+        { property: "og:url", content: "https://iqbalperkasa.com" },
+        { property: "og:type", content: "article" },
       ],
-      link: [
-        { rel: 'icon', type: 'image/png', href: '/avatar.png' },
-      ],
+      link: [{ rel: "icon", type: "image/png", href: "/avatar.png" }],
       script: [
         // { async: true, src: 'https://platform.twitter.com/widgets.js', },
-        { src: '/main.js' },
+        { src: "/main.js" },
       ],
     },
   },
   googleFonts: {
     families: {
-      Inter: '400..600',
+      Inter: "400..500",
     },
     download: true,
   },
   icon: {
     serverBundle: {
-      collections: ['gravity-ui'],
+      collections: ["gravity-ui"],
     },
   },
-})
+});
